ce93f74dd79d6de7578d654358d1a69786a9b494
TK-71421 <79209102+TK-71421@users.noreply.github.com>
2023-12-10 20:01:57 -0600

    added Twitter post functionality

--- dbConnector.py
+++ dbConnector.py
@@ -3,9 +3,25 @@ import mysql.connector
 from mysql.connector import errorcode
 
 # Load JSON data from file
-with open('C:/Users/Tengis/JupyterLabProjects/apiResponse.json', 'r') as json_file:
-    data = json.load(json_file)
+choice = input("Select social media sata to insert into database (will default to Reddit):\n1)Reddit\n2)Twitter\n3)Facebook\n")
 
+if(choice == '1'):
+    print("Loading Reddit post data...")
+    with open('C:/Users/Tengis/JupyterLabProjects/apiResponse.json', 'r') as json_file:
+        data = json.load(json_file)
+elif (choice == '2'):
+    print("Loading Twitter post data...")
+    with open('C:/Users/Tengis/JupyterLabProjects/liveTweets.json', 'r') as json_file:
+        data = json.load(json_file)
+elif (choice == '3'):
+    print("Loading Facebook post data...")
+    exit()
+else:
+    print("Input Error issue\nexiting...")
+    exit()
+
+
+#get Mysql credentials
 with open('MysqlRootPwd.txt', 'r') as file:
     credentials = [line.rstrip() for line in file]
 
@@ -15,7 +31,7 @@ try:
         host='localhost',
         user= credentials[0],
         password= credentials[1],
-        database='reddit_db'
+        database='social_media_data'
     )
 except mysql.connector.Error as err:
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
@@ -26,7 +42,6 @@ except mysql.connector.Error as err:
         print(err)
 
 
-
 # Create a cursor
 cursor = connection.cursor()
 
@@ -34,14 +49,27 @@ cursor = connection.cursor()
 if connection.is_connected():
     print("DB Connection Succesful...")
 
-# Insert data into MySQL table
-for post in data:
-    query = "INSERT INTO posts (title, subreddit, full_name, id, ups, downs, upvote_ratio, score, created_utc) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
-    values = (data[post]['title'], data[post]['subreddit_name_prefixed'], data[post]['name'], data[post]['id'], data[post]['ups'], data[post]['downs'], data[post]['upvote_ratio'], data[post]['score'], data[post]['created_utc'])
-    cursor.execute(query, values)
+# Insert data into MySQL table; this method protects against sql inject
+if(choice == '1'):
+    #insert Reddit Data
+    for post in data:
+        query = "INSERT INTO reddit_posts (title, subreddit, full_name, id, ups, downs, upvote_ratio, score, created_utc) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        values = (data[post]['title'], data[post]['subreddit_name_prefixed'], data[post]['name'], data[post]['id'], data[post]['ups'], data[post]['downs'], data[post]['upvote_ratio'], data[post]['score'], data[post]['created_utc'])
+        cursor.execute(query, values)
+elif(choice == '2'):
+    #Insert Twitter data
+    for post in data:
+        query = "INSERT INTO twitter_posts (title, date_posted, comments, retweets, quotes, likes, profile_id) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+        values = (data[post]['text'], data[post]['date'], data[post]['stats']['comments'], data[post]['stats']['retweets'], data[post]['stats']['quotes'], data[post]['stats']['likes'], data[post]['user']['profile_id'])
+        cursor.execute(query, values)
+else:
+    #Insert Facebook data
+    pass
 
+print(cursor)
 # Commit changes and close connection
 connection.commit()
+print("Database changes commited!")
 cursor.close()
 connection.close()
 
